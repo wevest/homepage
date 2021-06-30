7ab3fcf6bf6692a6d42d3d6be4c8358e9cbb893a
Kevin <kevin.yoo@edenchain.io>
2021-06-30 17:31:39 +0900

    merge

--- src/components/LoadMore.vue
+++ src/components/LoadMore.vue
@@ -1,10 +1,11 @@
 <template>
     
     <div>
+        <q-separator class="loadmoreSeparator1" size="1px" />
 		<div class="loadMoreBox" v-if="v_next_url">
             <q-btn class="expandMore" icon-right="expand_more" label="Loadmore" dense flat @click="onClickLoadMore" />
 		</div>    
-        <q-separator class="loadmoreSeparator" size="1px" />
+        <q-separator class="loadmoreSeparator1" size="1px" />
     </div>
 
 </template>
@@ -50,10 +51,13 @@ export default {
 </script>
 
 
-<style scope>
+<style scope>s
 
+.loadmoreSeparator1 {
+    margin-bottom:15px;
 
-.loadmoreSeparator {
+}
+.loadmoreSeparator2 {
     margin-top:15px;
 
 }
