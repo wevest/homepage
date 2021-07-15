616a5bc9b94084c1839807404efa4701f3a2b9d1
james.ahn <38304972+Edenpartners@users.noreply.github.com>
2021-07-15 17:19:39 +0900

    review ui updated

--- src/pages/assetreview/component/ReviewRatingBox.vue
+++ src/pages/assetreview/component/ReviewRatingBox.vue
@@ -1,39 +1,56 @@
 <template>
     <div>
         <CTitle ttype='subtitle' :title="title" desc=""></CTitle>          
-        <div>
-            <q-rating
-                name="quality"
-                v-model="reviews.stat.average_rating"
-                max="5"
-                size="3.5em"
-                color="yellow"
-                icon="star_border"
-                icon-selected="star"
-                no-dimming
-                v-if="reviews.stat.average_rating"
-            /> 
-            <span>{{reviews.stat.average_rating}}</span>
+        <div class="row q-my-md">
+            <div class="col-6 q-px-sm">
+                <div>
+                    Review Rating
+                </div>
+                <div>
+                    <span>{{reviews.stat.average_rating}}</span>
+                </div>
+                <q-rating
+                    name="quality"
+                    v-model="reviews.stat.average_rating"
+                    max="5"
+                    size="1.5em"
+                    color="yellow"
+                    icon="star_outline"
+                    icon-selected="star"
+                    no-dimming
+                    v-if="reviews.stat.average_rating"
+                />                 
+            </div>
+            <div class="col-6 q-px-sm">
+                <div>
+                    Rating Ratio
+                </div>
+                
+                <div>
+                    <div class="boxRating" v-for="index in v_loop">
+                        <div class="boxLabel">
+                            <span>{{index}}</span>
+                        </div>
+                        <div class="boxProgress">
+                            <q-linear-progress stripe rounded size="15px" :value="v_value(index)" color="secondary" class="q-mt-sm" >
+                                <div class="flex flex-center">
+                                    <q-badge color="white" text-color="accent" :label="v_label(index)" />
+                                </div>
+                            </q-linear-progress>
+                        </div>
+                    </div>
+
+                </div>
+<!--                            
+-->                            
+            </div>
+
         </div>
+<!--        
         <div>
             {{reviews.stat.total_count}} ratings
         </div>
-
-        <div>
-            <div v-for="index in v_loop" class="boxRating">
-                <div class="boxLabel">
-                    <span>{{index}} Star</span>
-                </div>
-                <div class="boxProgress">
-                    <q-linear-progress stripe rounded size="15px" :value="v_value(index)" color="secondary" class="q-mt-sm" >
-
-                        <div class="flex flex-center">
-                            <q-badge color="white" text-color="accent" :label="v_label(index)" />
-                        </div>
-                    </q-linear-progress>
-                </div>
-            </div>
-        </div>
+-->
     </div>
 
 </template>
@@ -76,7 +93,9 @@ export default {
         },
         v_value() {
             return (index) => {   
-                return this.reviews.stat['rating_'+index]/100;
+                const value = this.reviews.stat['rating_'+index]/100;
+                logger.log.debug("v_value=",index,value);
+                return value;
             }
         },
 	},
@@ -100,7 +119,7 @@ export default {
 }
 
 .boxLabel {
-    width:70px;
+    width:20px;
 }
 
 .boxProgress {    
