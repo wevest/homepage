d7063b32e165ac8b2162781521fe6a59ab3e02dd
Kevin-1002 <kevin.yoo@edenchain.io>
2021-09-02 20:53:26 +0900

    merge

--- src/pages/pool/PoolView.vue
+++ src/pages/pool/PoolView.vue
@@ -318,6 +318,64 @@
             </div>
         </div>
 <!-- 유동성제거-page-END -->
+<!-- pool-detailed-page-시작 -->
+        <div class="">
+            <CTitle ttype='title' 
+                :title="$t('page.pool.title')" :desc="$t('')" 
+                extraCaption="Add" @onClickTitleExtra="onClickAdd" />
+        </div>   
+
+        <div class="row justify-center q-my-md">
+            <div class="q-mx-xs"> 
+                <q-select style="width: 160px" outlined v-model="model" :options="options" :label="$t('button.coinSelect')"  />
+            </div>
+            <q-space />
+            <div class="q-mt-sm"> 
+                <q-btn color="primary" :label="$t('button.add')" />
+            </div>
+        </div>
+        <div class="gBoarder2 q-mb-sm q-pa-md"> 
+            <div class="q-mb-sm">
+                {{ $t('page.pool.poolRatio.title') }}
+            </div>
+            <div class="full-width">
+                <q-btn-toggle
+                v-model="v_utilization"
+                toggle-color="primary"
+                spread
+                :options="[
+                    {label: '0%', value: '0'},
+                    {label: '25%', value: '25'},
+                    {label: '50%', value: '50'},
+                    {label: '75%', value: '75'}, 
+                    {label: '100%', value: '100'} 
+                    ]" />
+            </div>   
+            <div class="q-mt-md">
+                <q-badge color="secondary">
+                        Model: {{ basicModel }} (0 to 100, step 5)
+                        </q-badge>
+                        <q-slider
+                        v-model="basicModel"
+                        :step="5"
+                        label-always
+                        color="primary"
+                        />
+            </div>
+        </div>
+        <div class="gBoarder2 text-center">
+            <div class="q-my-lg"> 
+                <q-icon color="grey-7" size="60px" name="move_to_inbox" />
+            </div>
+            <div class="q-my-md gCaption"> 
+               {{ $t('page.pool.poolListHere.title..') }}
+            </div>
+            <div class="q-my-lg">  
+                <q-btn color="primary" rounded :label="$t('button.connectWallet')" />
+            </div>
+        </div>
+<!-- pool-detailed-page-끝 -->
+
     </div>
 </template>
 
